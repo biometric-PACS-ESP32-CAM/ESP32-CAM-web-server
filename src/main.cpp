2a21ae4646746b8f404485f75c1e917a10ae70b8
@ivan_xoxol <ivan_mischenko@niuitmo.ru>
2024-01-21 18:57:56 +0300

    chng cam server work

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,143 @@
 #include <Arduino.h>
+#include <esp32cam.h>
+#include <WebServer.h>
+#include <WiFi.h>
 
-#define AP_SSID "ssid"
-#define AP_PASS "pass"
+const char* WIFI_SSID = "MF_HOUSE_YKKI";
+const char* WIFI_PASS = "$@ndr0nix";
 
-#define FLASH_PIN 4
+WebServer server(80);
 
-#include "camera.h"
-#include <CamStream.h>  // входит в GyverPortal
+static auto loRes = esp32cam::Resolution::find(320, 240);
+static auto hiRes = esp32cam::Resolution::find(800, 600);
 
-#include <GyverPortal.h>
-GyverPortal ui;
+void handleBmp()
+{
+  if (!esp32cam::Camera.changeResolution(loRes)) {
+    Serial.println("SET-LO-RES FAIL");
+  }
 
-void build() {
-  // ширина колонки 500 пикс
-  GP.BUILD_BEGIN(GP_DARK, 500);
-  
-  // вывести окно стрима шириной 500 пикс
-  GP.CAM_STREAM(500);
-  
-  GP.LABEL("Flash:");
-  GP.SWITCH("flash");
-  GP.BUILD_END();
+  auto frame = esp32cam::capture();
+  if (frame == nullptr) {
+    Serial.println("CAPTURE FAIL");
+    server.send(503, "", "");
+    return;
+  }
+  Serial.printf("CAPTURE OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
+                static_cast<int>(frame->size()));
+
+  if (!frame->toBmp()) {
+    Serial.println("CONVERT FAIL");
+    server.send(503, "", "");
+    return;
+  }
+  Serial.printf("CONVERT OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
+                static_cast<int>(frame->size()));
+
+  server.setContentLength(frame->size());
+  server.send(200, "image/bmp");
+  WiFiClient client = server.client();
+  frame->writeTo(client);
 }
 
-void action() {
-  if (ui.click("flash")) digitalWrite(FLASH_PIN, ui.getBool());
+void serveJpg()
+{
+  auto frame = esp32cam::capture();
+  if (frame == nullptr) {
+    Serial.println("CAPTURE FAIL");
+    server.send(503, "", "");
+    return;
+  }
+  Serial.printf("CAPTURE OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
+                static_cast<int>(frame->size()));
+
+  server.setContentLength(frame->size());
+  server.send(200, "image/jpeg");
+  WiFiClient client = server.client();
+  frame->writeTo(client);
 }
 
-void startup() {
+void handleJpgLo()
+{
+  if (!esp32cam::Camera.changeResolution(loRes)) {
+    Serial.println("SET-LO-RES FAIL");
+  }
+  serveJpg();
+}
+
+void handleJpgHi()
+{
+  if (!esp32cam::Camera.changeResolution(hiRes)) {
+    Serial.println("SET-HI-RES FAIL");
+  }
+  serveJpg();
+}
+
+void handleJpg()
+{
+  server.sendHeader("Location", "/cam-hi.jpg");
+  server.send(302, "", "");
+}
+
+void handleMjpeg()
+{
+  if (!esp32cam::Camera.changeResolution(hiRes)) {
+    Serial.println("SET-HI-RES FAIL");
+  }
+
+  Serial.println("STREAM BEGIN");
+  WiFiClient client = server.client();
+  auto startTime = millis();
+  int res = esp32cam::Camera.streamMjpeg(client);
+  if (res <= 0) {
+    Serial.printf("STREAM ERROR %d\n", res);
+    return;
+  }
+  auto duration = millis() - startTime;
+  Serial.printf("STREAM END %dfrm %0.2ffps\n", res, 1000.0 * res / duration);
+}
+
+void setup()
+{
   Serial.begin(115200);
+  Serial.println();
+
+  {
+    using namespace esp32cam;
+    Config cfg;
+    cfg.setPins(pins::AiThinker);
+    cfg.setResolution(hiRes);
+    cfg.setBufferCount(2);
+    cfg.setJpeg(80);
+
+    bool ok = Camera.begin(cfg);
+    Serial.println(ok ? "CAMERA OK" : "CAMERA FAIL");
+  }
+
+  WiFi.persistent(false);
   WiFi.mode(WIFI_STA);
-  WiFi.begin(AP_SSID, AP_PASS);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
   }
+
+  Serial.print("http://");
   Serial.println(WiFi.localIP());
+  Serial.println("  /cam.bmp");
+  Serial.println("  /cam-lo.jpg");
+  Serial.println("  /cam-hi.jpg");
+  Serial.println("  /cam.mjpeg");
 
-  pinMode(FLASH_PIN, OUTPUT);
+  server.on("/cam.bmp", handleBmp);
+  server.on("/cam-lo.jpg", handleJpgLo);
+  server.on("/cam-hi.jpg", handleJpgHi);
+  server.on("/cam.jpg", handleJpg);
+  server.on("/cam.mjpeg", handleMjpeg);
+
+  server.begin();
 }
 
-void setup() {
-  startup();
-
-  // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
-  if (cam_init(FRAMESIZE_VGA)) Serial.println("Camera OK");
-  else Serial.println("Camera Error");
-
-  // начать стрим
-  cam_stream_begin();
-
-  ui.attachBuild(build);
-  ui.attach(action);
-  ui.start();
-}
-
-void loop() {
-  ui.tick();
+void loop()
+{
+  server.handleClient();
 }
